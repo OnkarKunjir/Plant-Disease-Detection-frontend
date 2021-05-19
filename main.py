8eafe57ecb2d920ad4397fabebcb808fc06c7721
OnkarKunjir <onkarkunjir8@gmail.com>
2021-05-19 10:13:40 +0530

    adding flask server.

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/try-now", methods=["GET"])
+def try_now():
+    return render_template("try_now.html")
+
+
+@app.route("/analyse", methods=["GET"])
+def analyse():
+    # TODO: Check if user is logged in or not.
+    return render_template("analyse.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
